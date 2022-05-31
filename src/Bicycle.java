b02e58f59ef426369706bb52afde8f1e4bee3bc6
Anostosia117 <jigsa@yandex.ru>
2022-06-01 01:41:16 +0300

    готово

---
+++ src/Bicycle.java
@@ -0,0 +1,15 @@
+public class Bicycle extends Vehicle implements ServiceInterface{
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+@Override
+    public void check() {
+        System.out.println("Обслуживаем " + getModelName());
+        for (int i = 0; i < getWheelsCount(); i++) {
+            updateTyre();
+        }
+    }
+}
---
+++ src/ServiceStation.java
@@ -0,0 +1,6 @@
+public class ServiceStation {
+    public void check(ServiceInterface serviceInterface) {
+        serviceInterface.check();
+    }
+
+}
